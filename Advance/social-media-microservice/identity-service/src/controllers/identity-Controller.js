1617080cfc85ad75681b9a5642bb393a40267d60
Muhammad Aqib <mohammadaaqib77@gmail.com>
2025-02-02 22:58:06 +0500

    adding logout and refresh token identity service completed

--- Advance/social-media-microservice/identity-service/src/controllers/identity-Controller.js
+++ Advance/social-media-microservice/identity-service/src/controllers/identity-Controller.js
@@ -1,6 +1,7 @@
 const logger = require("../utils/logger");
 const { validateRegester, validateLogin } = require("../utils/validation");
 const User = require("../models/User");
+const RefreshToken = require("../models/RefreshToken");
 const generateToken = require("../utils/generateToken");
 
 //user register
@@ -85,11 +86,6 @@ const loginUser = async (req, res) => {
       accesstoken,
       refreshToken,
     });
-
-
-
-
-
   } catch (error) {
     logger.error("Login user error occure", error);
     res.status(500).json({
@@ -100,7 +96,83 @@ const loginUser = async (req, res) => {
 };
 
 //refersh token
+const refreshTokenUser = async (req, res) => {
+  logger.info("Refresh token endpoint hit..");
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      logger.warn("Refresh Token missing");
+      res.status(400).json({
+        success: false,
+        message: "Refresh Token missing",
+      });
+    }
+
+    const storedToken = await RefreshToken.findOne({ token: refreshToken });
+    if (!storedToken || storedToken.expireAt < new Date()) {
+      logger.warn("Invalid or expire refresh token");
+      res.status(401).json({
+        success: false,
+        message: "Invalid or expire refresh token",
+      });
+    }
+
+    const user = await User.findById(storedToken.user);
+
+    if (!user) {
+      logger.warn("User not found");
+      res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const { accesstoken: newAccessToken, refreshToken: newRefreshToken } =
+      await generateToken(user);
+
+    //delete previos token
+    await RefreshToken.deleteOne({ _id: storedToken._id });
+
+    res.status(200).json({
+      accesstoken: newAccessToken,
+      refreshToken: newRefreshToken,
+    });
+  } catch (error) {
+    logger.error("Refresh token error occure", error);
+    res.status(500).json({
+      success: false,
+      message: "internal error",
+    });
+  }
+};
 
 //logout
+const logoutUser = async (req, res) => {
+  logger.info("logout user  endpoint hit..");
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      logger.warn("Refresh Token missing");
+      res.status(400).json({
+        success: false,
+        message: "Refresh Token missing",
+      });
+    }
 
-module.exports = { registerUser,loginUser };
+    await RefreshToken.deleteOne({ token: refreshToken });
+    logger.info("RefreshToken is deleted for logout");
+    res.json({
+      success: true,
+      message: "Logged Out successfully",
+    });
+  } catch (error) {
+    logger.error("LogOut  error occure", error);
+    res.status(500).json({
+      success: false,
+      message: "internal error",
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser, refreshTokenUser, logoutUser };
+ 
--- Advance/social-media-microservice/identity-service/src/routes/identity-service.js
+++ Advance/social-media-microservice/identity-service/src/routes/identity-service.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { registerUser,loginUser } = require("../controllers/identity-Controller");
+const {
+  registerUser,
+  loginUser,
+  refreshTokenUser,
+  logoutUser,
+} = require("../controllers/identity-Controller");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
-router.post("/login",loginUser );
+router.post("/login", loginUser);
+router.post("/refresh-token", refreshTokenUser);
+router.post("/logout", logoutUser);
 
 module.exports = router;

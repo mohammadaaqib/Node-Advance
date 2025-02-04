c49cabd3d026aeb09bf28efa0ad6f1c5831b0afb
Muhammad Aqib <mohammadaaqib77@gmail.com>
2025-02-04 14:54:24 +0500

    basic setup for media service

---
+++ Advance/social-media-microservice/media-service/src/utils/logger.js
@@ -0,0 +1,30 @@
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === "PROD" ? "debug" : "info",
+  //what will be the format of our log
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  //what is the service
+  defaultMeta: { service: "media-service" },
+  //where we need to print log
+  // 1 on console
+  // 2 in error than in error file
+  // 3 print all the logs
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+module.exports = logger;
